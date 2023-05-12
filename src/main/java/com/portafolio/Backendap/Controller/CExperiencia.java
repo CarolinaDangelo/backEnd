8ebea19f2dd221f2a16b9abe96015a6a804b9f86
Carola <carolinadangelo@outlook.com>
2023-05-11 21:56:17 -0300

    Run

--- src/main/java/com/portafolio/Backendap/Controller/CExperiencia.java
+++ src/main/java/com/portafolio/Backendap/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://portafolio-frontend-ap.web.app/"})
 public class CExperiencia {
 
     @Autowired
--- src/main/java/com/portafolio/Backendap/Controller/PersonaController.java
+++ src/main/java/com/portafolio/Backendap/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portafolio-frontend-ap.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
