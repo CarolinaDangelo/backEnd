abac089b97e73347bf4912c1ecc98e9d807bc31f
Carola <carolinadangelo@outlook.com>
2023-05-12 06:31:56 -0300

    Main Update

--- src/main/java/com/portafolio/Backendap/Security/MainSecurity.java
+++ src/main/java/com/portafolio/Backendap/Security/MainSecurity.java
@@ -50,7 +50,7 @@ public class MainSecurity {
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
+                .requestMatchers("**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
